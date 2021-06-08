3f5a82eaa9041ce6baee0f7da828131e773eaa55
Nikola <nikola.gudelj@uns.ac.rs>
2021-06-08 16:43:13 +0200

    Upload demo complete

--- src/services/storageService.js
+++ src/services/storageService.js
@@ -16,31 +16,73 @@ firebase.initializeApp(firebaseConfig);
 const imageRef = firebase.storage().ref().child('images');
 const videoRef = firebase.storage().ref().child('videos');
 
-function getImage(name) {
-    return imageRef.child(name);
+export {
+    upload, getUploadUrls
 }
 
-function getVideo(name) {
-    return videoRef.child(name);
+/**
+ *  Uploads the given FileList to the Cloud Storage.
+ *  @see FileList Only supported argument is FileList.
+ *  @return A list of promises which can be used to fetch snapshots of uploaded data.
+ */
+function upload(files) {
+    const snapshotPromises = [];
+
+    files.forEach(file => {
+        console.log('Uploading: [' + file.name + ']')
+        const ext = getExtension(file);
+
+        if (isImage(ext)) {
+            snapshotPromises.push(uploadImage(file, ext));
+        }
+        else if (isVideo(ext)) {
+            snapshotPromises.push(uploadVideo(file, ext));
+        }
+        else console.error('Unrecognized file type: [' + file.name + ']');
+    })
+
+    return snapshotPromises;
 }
 
-function postImage(file, name) {
-    return randomStorageRef(imageRef, getExtension(name)).put(file);
+/** Returns a promise for a list of promises, each child promise awaiting a String response */
+async function getUploadUrls(snapshotPromises) {
+    const promises = [];
+
+    for (const snapshotPromise of snapshotPromises) {
+        const snapshot = await snapshotPromise;
+        promises.push(snapshot.ref.getDownloadURL());
+    }
+
+    return promises;
 }
 
-function postVideo(file) {
-    return randomStorageRef(videoRef).put(file);
+/** Uploads an image to the Cloud Storage. Returns a snapshot promise. */
+function uploadImage(file, ext) {
+    return randomStorageRef(imageRef, ext).put(file);
 }
 
+/** Uploads a video to the Cloud Storage. Returns a snapshot promise. */
+function uploadVideo(file, ext) {
+    return randomStorageRef(videoRef, ext).put(file);
+}
+
+/** Generates a random UUID Cloud Storage reference. */
 function randomStorageRef(storageRef, extension) {
     return storageRef.child(uuidv4() + '.' + extension);
 }
 
-function getExtension(name) {
-    let tokens = name.split('.');
+/** Returns the extension for the given File object argument */
+function getExtension(file) {
+    let tokens = file.name.split('.');
     return tokens[tokens.length - 1];
 }
 
-export {
-    getImage, getVideo, postImage, postVideo
+/** Checks whether the given file extension represents an image. */
+function isImage(ext) {
+    return ['png', 'jpg', 'jpeg'].includes(ext.toLowerCase());
+}
+
+/** Checks whether the given file extension represents a video */
+function isVideo(ext) {
+    return ['mp4'].includes(ext.toLowerCase());
 }
--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,43 +1,27 @@
 <template>
   <div style="margin: 50px auto 0;">
-    <input id="image-upload" type="file" placeholder="Upload file">
-    <button style="background-color: green; margin-top:20px;" @click="upload">Upload image</button>
-    <img src="" alt="An image" id="an-image"/>
+    <input id="image-upload" multiple type="file" placeholder="Upload file">
   </div>
 </template>
 
 <script>
 
 
-import {postImage} from "@/services/storageService";
+import {getUploadUrls, upload} from "@/services/storageService";
 
 export default {
     name: "Test",
-    methods: {
-        upload() {
-            let img = document.getElementById("an-image");
-
-            fetch(img.src)
-              .then(res => res.blob())
-                  .then(blobResponse => {
-                      postImage(blobResponse, img.name)
-                          .then(storageResponse => {
-                              storageResponse.ref.getDownloadURL()
-                                  .then(url => console.log(url))
-                          })
-                  });
-        }
-    },
     mounted() {
         document.getElementById('image-upload').onchange =
-            function(e) {
-                this.img = document.getElementById("an-image");
+            function (e) {
+                getUploadUrls(upload(e.target.files))
+                    .then(urlPromises => {
+                        urlPromises.forEach(urlPromise => {
+                            urlPromise.then(url => console.log(url));
+                        })
+                    })
 
-                let file = e.target.files[0]
-
-                this.img.src = window.URL.createObjectURL(file);
-                this.img.name = file.name;
-        }
+            }
     }
 }
 </script>
