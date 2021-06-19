e86e5153fe9ee6e0fef05e9ac463f32aa4c95cff
sStrbacki <sasa.strbacki@protonmail.com>
2021-06-19 22:02:29 +0200

    Frontend fixed

--- src/components/common/story/StoryView.vue
+++ src/components/common/story/StoryView.vue
@@ -1,61 +1,61 @@
 <template>
-  <v-slide-group class="pa-4" active-class="success" show-arrows>
-    <v-slide-item
-        v-for="story in stories"
-        :key="story.id"
-    >
-      <v-card class="ma-4" height="530" width="500">
-        <post-card-small
-            :post="story.post"
-            v-if="story.reshare"
-        />
-        <v-img
-            contain
-            v-else-if="!isVideo(story.mediaUrl)"
-            :src="story.mediaUrl"
-            height="300px"
-            aspect-ratio="1"
-        >
-        </v-img>
-        <video-player v-else>
-          <source :src="story.mediaUrl" />
-        </video-player>
-        <v-card-title>
-          <v-row>
-            <v-col cols="1">
-              <v-icon dark>
-                mdi-account-circle
-              </v-icon>
-            </v-col>
-            <v-col class="subtitle-2 mt-2">
-              <p>{{ story.author }}</p>
-            </v-col>
-          </v-row>
-        </v-card-title>
-        <v-card-subtitle>
-          <v-row>
-            <v-col cols="1">
-              <v-icon dark>
-                mdi-closed-caption
-              </v-icon>
-            </v-col>
-            <v-col>
-              {{ story.caption }}
-            </v-col>
-          </v-row>
-          <v-row v-if="story.location">
-            <v-col cols="1">
-              <v-icon dark>
-                mdi-map-marker
-              </v-icon>
-            </v-col>
-            <v-col>
-              {{ story.location.name }}
-            </v-col>
-          </v-row>
-        </v-card-subtitle>
+	<v-slide-group class="pa-4" active-class="success" show-arrows center-active>
+		<v-slide-item v-for="story in stories" :key="story.id">
+			<v-card class="ma-4" height="600" width="500">
+				<post-card-small :post="story.post" v-if="story.reshare" />
+				<v-img
+					contain
+					v-else-if="!isVideo(story.mediaUrl)"
+					:src="story.mediaUrl"
+					height="300px"
+					aspect-ratio="1"
+				>
+				</v-img>
+				<video-player v-else>
+					<source :src="story.mediaUrl" />
+				</video-player>
+				<v-card-title>
+					<v-row>
+						<v-col cols="1">
+							<v-icon dark>
+								mdi-account-circle
+							</v-icon>
+						</v-col>
+						<v-col class="subtitle-2 mt-2">
+							<p>{{ story.author }}</p>
+						</v-col>
+					</v-row>
+				</v-card-title>
+				<v-card-subtitle>
+					<v-row>
+						<v-col cols="1">
+							<v-icon dark>
+								mdi-closed-caption
+							</v-icon>
+						</v-col>
+						<v-col>
+							{{ story.caption }}
+						</v-col>
+					</v-row>
+					<v-row v-if="story.location">
+						<v-col cols="1">
+							<v-icon dark>
+								mdi-map-marker
+							</v-icon>
+						</v-col>
+						<v-col>
+							{{ story.location.name }}
+						</v-col>
+					</v-row>
+				</v-card-subtitle>
 				<v-card-actions v-if="story.author === currentUser">
-					<v-btn small color="info" v-if="!editStoryId" @click="selectStory(story.id)">Add as highlight</v-btn>
+					<v-btn
+						small
+						color="info"
+						v-if="!editStoryId"
+						@click="selectStory(story.id)"
+						>Add as highlight</v-btn
+					>
 					<div class="d-flex align-center" v-if="editStoryId === story.id">
 						<v-select
 							placeholder="Select existing highlight"
@@ -71,83 +71,93 @@
 							v-model="highlightInput"
 							@keydown="selectedHighlight = null"
 						></v-text-field>
-						<v-btn color="primary" icon @click="addHighlight()"><v-icon>mdi-check</v-icon></v-btn>
-						<v-btn color="warning" icon @click="cancelHighlight()"><v-icon>mdi-close</v-icon></v-btn>
+						<v-btn color="primary" icon @click="addHighlight()"
+							><v-icon>mdi-check</v-icon></v-btn
+						>
+						<v-btn color="warning" icon @click="cancelHighlight()"
+							><v-icon>mdi-close</v-icon></v-btn
+						>
 					</div>
 				</v-card-actions>
-      </v-card>
-    </v-slide-item>
-  </v-slide-group>
+			</v-card>
+		</v-slide-item>
+	</v-slide-group>
 </template>
 
 <script>
-import { addStoryToHighlight, createHighlight } from '@/services/contentService';
+import {
+	addStoryToHighlight,
+	createHighlight
+} from '@/services/contentService';
 import { notifyError } from '@/services/notificationService';
+import PostCardSmall from '../../../components/user/feed/PostCardSmall.vue';
 
-	export default {
-    name: 'StoryView',
-    props: {
-      stories: Array
-    },
-		data: () => {
-    	return {
-				editStoryId: '',
-				highlightInput: '',
-				selectedHighlight: null
-			}
+export default {
+	name: 'StoryView',
+	props: {
+		stories: Array
+	},
+	components: { PostCardSmall },
+	data: () => {
+		return {
+			editStoryId: '',
+			highlightInput: '',
+			selectedHighlight: null
+		};
+	},
+	mounted() {
+		this.$store.dispatch('getViewingProfileHighlights', this.currentUser);
+	},
+	methods: {
+		isVideo(url) {
+			return url.includes('videos');
 		},
-		mounted() {
-    	this.$store.dispatch('getViewingProfileHighlights', this.currentUser);
+		selectStory(id) {
+			this.editStoryId = id;
 		},
-    methods: {
-      isVideo(url) {
-        return url.includes('videos');
-      },
-			selectStory(id) {
-      	this.editStoryId = id;
-			},
-			highlightId: highlight => highlight.id,
-			highlightName: highlight => highlight.name,
-			cancelHighlight() {
-      	this.editStoryId = '';
-      	this.selectedHighlight = null;
-      	this.highlightInput = '';
-			},
-			async addHighlight() {
-      	if (this.highlightInput) {
-					const response = await createHighlight(this.highlightInput)
-					if (response.status >= 400) {
-						notifyError(response.data);
-					} else {
-						await this.addStoryToHighlight(this.editStoryId, response.data.id);
-					}
+		highlightId: highlight => highlight.id,
+		highlightName: highlight => highlight.name,
+		cancelHighlight() {
+			this.editStoryId = '';
+			this.selectedHighlight = null;
+			this.highlightInput = '';
+		},
+		async addHighlight() {
+			if (this.highlightInput) {
+				const response = await createHighlight(this.highlightInput);
+				if (response.status >= 400) {
+					notifyError(response.data);
 				} else {
-      		await this.addStoryToHighlight(this.editStoryId, this.selectedHighlight);
+					await this.addStoryToHighlight(this.editStoryId, response.data.id);
 				}
-      	this.cancelHighlight();
-			},
-			async addStoryToHighlight(storyId, highlightId) {
-				const res = await addStoryToHighlight(storyId, highlightId);
-				if (res.status >= 400) {
-					notifyError(res.data);
-				}
+			} else {
+				await this.addStoryToHighlight(
+					this.editStoryId,
+					this.selectedHighlight
+				);
 			}
-    },
-		computed: {
-    	currentUser: {
-    		get() {
-    			return this.$store.getters.username;
-				}
-			},
-			highlights: {
-    		get() {
-    			return this.$store.getters.viewingProfileHighlights;
-				}
+			this.cancelHighlight();
+		},
+		async addStoryToHighlight(storyId, highlightId) {
+			const res = await addStoryToHighlight(storyId, highlightId);
+			if (res.status >= 400) {
+				notifyError(res.data);
 			}
 		}
-  }
+	},
+	computed: {
+		currentUser: {
+			get() {
+				return this.$store.getters.username;
+			}
+		},
+		highlights: {
+			get() {
+				return this.$store.getters.viewingProfileHighlights;
+			}
+		}
+	}
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
